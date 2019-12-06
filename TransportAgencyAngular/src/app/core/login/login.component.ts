c03684aad4c83cebbc129be93cf7222e4f8f9f69
HiKoLaaa <hikolaaa2017@yandex.ru>
2019-12-07 01:48:43 +0300

    Add: класс хелпер для имён ролей + добавил задел под панель администратора.

--- TransportAgencyAngular/src/app/core/login/login.component.ts
+++ TransportAgencyAngular/src/app/core/login/login.component.ts
@@ -1,6 +1,8 @@
 import {Component} from '@angular/core';
 import {AuthService} from '../../service/auth/auth.service';
 import {LoginInfoFormGroup} from '../../form/login/login-info.form-group';
+import {AccountRepository} from '../../model/repository/accountRepository.model';
+import {RoleNameHelper} from '../../model/helper/role-name.helper';
 
 @Component({
   selector: 'app-login',
@@ -12,13 +14,16 @@ export class LoginComponent {
   form: LoginInfoFormGroup;
   submit: boolean;
   login: string;
+  admin: boolean;
 
-  constructor(private auth: AuthService) {
+  constructor(private auth: AuthService,
+              private accountRepository: AccountRepository) {
     this.form = new LoginInfoFormGroup();
-    this.login = auth.loginName;
+    this.login = auth.loginName === null ? '' : auth.loginName.toLowerCase();
     this.submit = false;
     if (auth.jwToken) {
       this.isSuccessLogIn = true;
+      this.isAdmin();
     }
   }
 
@@ -35,9 +40,10 @@ export class LoginComponent {
     if (this.form.valid) {
       this.auth.login(this.form.get('login').value, this.form.get('password').value)
         .subscribe(success => {
-          this.login = this.form.get('login').value;
+          this.login = this.form.get('login').value.toString().toLowerCase();
           this.isSuccessLogIn = success;
           if (this.isSuccessLogIn) {
+            this.isAdmin();
             this.form.reset();
             this.submit = false;
           }
@@ -49,5 +55,10 @@ export class LoginComponent {
     this.auth.logout();
     this.isSuccessLogIn = undefined;
     this.submit = false;
+    this.admin = undefined;
+  }
+
+  private isAdmin() {
+    this.accountRepository.getRole().subscribe(role => this.admin = role === RoleNameHelper.ADMIN_ROLE);
   }
 }
---
+++ TransportAgencyAngular/src/app/model/helper/role-name.helper.ts
@@ -0,0 +1,4 @@
+export class RoleNameHelper {
+  static readonly ADMIN_ROLE: string = 'Admin';
+  static readonly USER_ROLE: string = 'User';
+}
