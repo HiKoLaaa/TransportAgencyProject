724a12bda607972307573d09eb7bda529bd0cbfd
HiKoLaaa <hikolaaa2017@yandex.ru>
2019-12-14 01:26:27 +0300

    Add: guard для типов транспорта.

--- TransportAgencyAngular/src/app/guard/guard.module.ts
+++ TransportAgencyAngular/src/app/guard/guard.module.ts
@@ -1,10 +1,11 @@
 import {NgModule} from '@angular/core';
 import {ModelModule} from '../model/model.module';
 import {CanActivateOnlyMainPageGuard} from './can-activate-all-but-main-page/can-activate-only-main-page-guard.service';
+import {ResolveTransportTypeInfoGuard} from './resolve-transport-type-info/resolve-transport-type-info.guard';
 import {ResolveTripInfoGuard} from './resolve-trip-info/resolve-trip-info.guard';
 
 @NgModule({
   imports: [ModelModule],
-  providers: [ResolveTripInfoGuard, CanActivateOnlyMainPageGuard]
+  providers: [ResolveTripInfoGuard, CanActivateOnlyMainPageGuard, ResolveTransportTypeInfoGuard]
 })
 export class GuardModule { }
