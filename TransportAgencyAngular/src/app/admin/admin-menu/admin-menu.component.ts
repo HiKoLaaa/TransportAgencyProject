3a930e2aaddbb5e5342d0368dbfa16be958951af
HiKoLaaa <hikolaaa2017@yandex.ru>
2019-12-19 22:22:02 +0300

    Revert "Refactor: избавился от дублирования html разметки путём добавления общего шаблона (компонента) и изменения маршрутизации."
    This reverts commit ee43cf742b05f040f8fd8ee70a776ff7320f07e3.

--- TransportAgencyAngular/src/app/admin/admin-menu/admin-menu.component.ts
+++ TransportAgencyAngular/src/app/admin/admin-menu/admin-menu.component.ts
@@ -12,7 +12,7 @@ export class AdminMenuComponent {
   constructor(private location: Location) {
     this.administratingEntities = [
       ['Рейсы', 'trips'],
-      ['Типы транспорта', 'transport_types'],
+      ['Типы транспорта', 'transportTypes'],
       ['Места', 'places'],
       ['Страны', 'countries'],
       ['Покупатели', 'customers']
